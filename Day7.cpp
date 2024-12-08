56f35f2fa37384ce94b01e18c61f0b658d6eb978
Juanma <frikychua@hotmail.es>
2024-12-08 21:31:42 +0100

    day 7 part 2

--- Day7.cpp
+++ Day7.cpp
@@ -4,12 +4,24 @@
 
 #include "Day7.h"
 
+#include <algorithm>
 #include <bitset>
 #include <fstream>
 #include <iostream>
 #include <sstream>
 #include <vector>
 #include <cmath>
+
+std::string Day7::intToBase3(int integer) {
+    std::string chars = "0123456789abcdefghijklmnopqrstuvwxyz";
+    std::string s;
+    do{
+        s.append(chars.substr(integer % 3,1));
+        integer = integer / 3;
+    } while(integer> 0);
+    //std::reverse(s.begin(),s.end());
+    return s;
+}
 
 std::vector<char> Day7::generaPermutacion(int combination,const int &size) {
     std::bitset<64> bitsPermutation = combination;
@@ -22,6 +34,28 @@ std::vector<char> Day7::generaPermutacion(int combination,const int &size) {
             }
             if(bitsPermutation[j]== 1) {
                 vectorPermutation.emplace_back('*');
+            }
+        }else {
+            vectorPermutation.emplace_back('+');
+        }
+    }
+    return vectorPermutation;
+}
+
+std::vector<char> Day7::generaPermutacion2(int combination,const int &size) {
+    std::string bitsPermutation = intToBase3(combination);
+    std::vector<char> vectorPermutation;
+    vectorPermutation.reserve(size-1);
+    for(int i=0,j=size-1;i<size;i++,j--) {
+        if(j<bitsPermutation.size()) {
+            if(bitsPermutation[j]== '0') {
+                vectorPermutation.emplace_back('+');
+            }
+            if(bitsPermutation[j]== '1') {
+                vectorPermutation.emplace_back('*');
+            }
+            if(bitsPermutation[j]== '2') {
+                vectorPermutation.emplace_back('|');
             }
         }else {
             vectorPermutation.emplace_back('+');
@@ -75,13 +109,13 @@ void Day7::execute() {
     for(int rowIndex=0;rowIndex<results.size();rowIndex++) {
         int combination = 0;
         bool correctResult = false;
-        __int64 n = pow(2,numbers.at(rowIndex).size()-1);
+        __int64 n = pow(3,numbers.at(rowIndex).size()-1);
         //std::cout << "-------------------------------------------" << n << std::endl;
         while(combination < n && !correctResult){
             tempResult = 0;
             bool first = true;
             //operators.emplace_back();
-            auto operatorsPermutation = generaPermutacion(combination,numbers.at(rowIndex).size()-1);
+            auto operatorsPermutation = generaPermutacion2(combination,numbers.at(rowIndex).size()-1);
             for(int i=0,j=1;j<numbers.at(rowIndex).size();i++,j++) {
                 if(operatorsPermutation.at(i) == '*' && first) {
                     tempResult = numbers.at(rowIndex).at(i) * numbers.at(rowIndex).at(j);
@@ -95,12 +129,21 @@ void Day7::execute() {
                 if(operatorsPermutation.at(i) == '+' && !first) {
                     tempResult += numbers.at(rowIndex).at(j);
                 }
+                if(operatorsPermutation.at(i) == '|' && first) {
+                    tempResult = _atoi64((std::to_string(numbers.at(rowIndex).at(i)) + std::to_string(numbers.at(rowIndex).at(j))).c_str());
+                    //std::cout << numbers.at(rowIndex).at(i) << " || " << numbers.at(rowIndex).at(j) << " = " << tempResult << std::endl;
+                }
+                if(operatorsPermutation.at(i) == '|' && !first) {
+                    //std::cout << tempResult << " || " << numbers.at(rowIndex).at(j);
+                    tempResult = _atoi64((std::to_string(tempResult) + std::to_string(numbers.at(rowIndex).at(j))).c_str());
+                    //std::cout << " = " << tempResult << std::endl;
+                }
                 first = false;
                 //std::cout << tempResult << std::endl;
             }
             correctResult = correctResult || tempResult == results.at(rowIndex);
             if(tempResult == results.at(rowIndex)) {
-                std::cout << "-------------- Correct result: " << tempResult<< std::endl;
+                //std::cout << "-------------- Correct result: " << tempResult<< std::endl;
                 countCorrectResults = countCorrectResults + tempResult;
             }
             //std::cout << "-------------------------------------------" << std::endl;
--- Day7.h
+++ Day7.h
@@ -5,12 +5,19 @@
 #ifndef DAY7_H
 #define DAY7_H
 #include <vector>
+#include <bits/basic_string.h>
 #include <bits/c++config.h>
 
 
 class Day7 {
 public:
+    static std::string CHARS;
+
+    static std::string intToBase3(int integer);
+
     static std::vector<char> generaPermutacion(int combination, const int &size);
+
+    static std::vector<char> generaPermutacion2(int combination, const int &size);
 
     void static execute();
 };
